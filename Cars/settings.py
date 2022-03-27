ae711a8b1871538597b69e2f96dd257708cc9bac
HITANSHU PAREKH <hitanshuparekh72@gmail.com>
2022-03-27 18:28:27 +0530

    Settings File Updated

--- Cars/settings.py
+++ Cars/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u3addbnlq(-500$6mm-_a855^yo+l-ed#!cn+m%j1tso#e5ilp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
